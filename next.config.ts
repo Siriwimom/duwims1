958125eb28060b4098579bd6db3a81eba71167ca
Alberta00 <155601816+Siriwimom@users.noreply.github.com>
2025-12-13 18:00:34 +0700

    add all

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,12 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  output: "export",         // สำหรับ static export
+  output: "export", // ใช้แทน next export
   basePath: isProd ? "/duwims1" : "",
   assetPrefix: isProd ? "/duwims1/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
