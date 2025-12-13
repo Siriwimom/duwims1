81f63d061ab2b59c9af23d509cfb6b6a637c78d2
Alberta00 <155601816+Siriwimom@users.noreply.github.com>
2025-12-13 16:43:20 +0700

    Updates

--- _next/static/MHpaYa2ISCUZDs-3fxeRi/_buildManifest.js
+++ _next/static/MHpaYa2ISCUZDs-3fxeRi/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/duwims1//_next/:path+",
+        "destination": "/duwims1/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
